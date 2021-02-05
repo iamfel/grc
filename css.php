724a904e84252f5ed026146140e2a989e1807f33
iamfel <empene8@gmail.com>
2021-02-05 17:49:06 +0100

    first commit

---
+++ css.php
@@ -0,0 +1,28 @@
+<!-- Bootstrap CSS-->
+<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+
+<!-- Font-Awesome -->
+<link rel="stylesheet" type="text/css" href="css/font-awesome.css">
+
+<!-- Icomoon -->
+<link rel="stylesheet" type="text/css" href="css/icomoon.css">
+
+<!-- Animate.css -->
+<link rel="stylesheet" href="css/animate.css">
+
+<!-- Revolution Slider  -->
+<link rel="stylesheet" href="css/rev-settings.css">
+<link rel="stylesheet" href="css/slider.css">
+
+<!-- Color Switcher -->
+<link rel="stylesheet" type="text/css" href="css/switcher.css">
+
+<!-- Owl Carousel  -->
+<link rel="stylesheet" href="css/owl.carousel.css">
+
+<!-- Main Styles -->
+<link rel="stylesheet" type="text/css" href="css/default.css">
+<link rel="stylesheet" type="text/css" href="css/styles.css" id="colors">
+
+<!-- Fonts Google -->
+<link href="https://fonts.googleapis.com/css?family=Fira+Sans:100,200,300,400,500,600,700,800,900" rel="stylesheet">
---
+++ ncss.php
@@ -0,0 +1,31 @@
+<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+
+<!-- Font-Awesome -->
+<link rel="stylesheet" type="text/css" href="css/font-awesome.css">
+
+<!-- Icomoon -->
+<link rel="stylesheet" type="text/css" href="css/icomoon.css">
+
+<!-- Slider -->
+<link rel="stylesheet" type="text/css" href="css/swiper.min.css">
+<link rel="stylesheet" type="text/css" href="css/rev-settings.css">
+
+<!-- Animate.css -->
+<link rel="stylesheet" href="css/animate.css">
+
+<!-- Color Switcher -->
+<link rel="stylesheet" type="text/css" href="css/switcher.css">
+
+<!-- Owl Carousel  -->
+<link rel="stylesheet" href="css/owl.carousel.css">
+
+<!-- Slider -->
+<link rel="stylesheet" type="text/css" href="css/swiper.min.css">
+<link rel="stylesheet" type="text/css" href="css/slider.css">
+
+<!-- Main Styles -->
+<link rel="stylesheet" type="text/css" href="css/default.css">
+<link rel="stylesheet" type="text/css" href="css/styles.css" id="colors">
+
+<!-- Fonts Google -->
+<link href="https://fonts.googleapis.com/css?family=Fira+Sans:100,200,300,400,500,600,700,800,900" rel="stylesheet">
